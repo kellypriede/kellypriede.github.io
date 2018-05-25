090b3ae0768a8515395c94b43e383a9f605d5f15
Daisy Priede <daisypriede@Daisys-MacBook-Pro.local>
2018-05-25 16:27:44 -0400

    first

---
+++ mail.php
@@ -0,0 +1,49 @@
+<?php 
+if(isset($_POST['submit'])){
+    $to = "priede@bc.edu"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    // mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+?>
+
+<!DOCTYPE html>
+<head>
+<title>Form submission</title>
+</head>
+<body>
+
+<form action="" method="post">
+First Name: <input type="text" name="first_name"><br>
+Last Name: <input type="text" name="last_name"><br>
+Email: <input type="text" name="email"><br>
+Message:<br><textarea rows="5" name="message" cols="30"></textarea><br>
+<input type="submit" name="submit" value="Submit">
+</form>
+
+</body>
+</html> 
+
+<?php $fname = $_POST['fname'];
+$lname = $_POST['lname']; 
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$formcontent="From: $fname \n Message: $message";
+$recipient = "priede@bc.edu";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
